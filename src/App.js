29091492867f5e15f02dc121f6baec4fccf1d614
Ekin Alçar <ekinalcar@gmail.com>
2020-12-10 14:22:31 +0100

    added tailwind

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { map, filter } from "lodash";
+import { map } from "lodash";
 
 const App = () => {
   const [jobs, setJobs] = useState([]);
@@ -30,6 +30,7 @@ const App = () => {
         page++;
       } catch (error) {
         setError(error);
+        console.log(error);
       }
     }
 
@@ -42,9 +43,43 @@ const App = () => {
   }, []);
   console.log(jobs);
   return (
-    <div>
+    <div className="sm:container sm:mx-auto p-5">
+      {error && <p>ERROR ...</p>}
       {isLoading && <p>IS LOADING...</p>}
-      {jobs?.length > 0 && map(jobs, (job, i) => <p key={i}>{job.title}</p>)}
+      {jobs?.length > 0 && (
+        <div class="grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-1 gap-5 ">
+          {map(jobs, (job, i) => (
+            <div
+              key={i}
+              className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl"
+            >
+              <div className="md:flex">
+                <div className="md:flex-shrink-0">
+                  <img
+                    className="h-20 w-full object-contain md:w-20"
+                    src={job.company_logo}
+                    alt="Man looking at item at a store"
+                  />
+                </div>
+                <div className="px-2 py-4">
+                  <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+                    {job.title}
+                  </div>
+                  <a
+                    href={job.url}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+                  >
+                    {job.company}
+                  </a>
+                  <p className="mt-2 text-gray-500"></p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
