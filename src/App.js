547bcd714e47cf2645e20ca62b19ba6a3fd67d80
Ekin Alçar <ekinalcar@gmail.com>
2020-12-10 11:33:16 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { map, filter } from "lodash";
 
-function App() {
+const App = () => {
+  const [jobs, setJobs] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const fetchGithub = async () => {
+    let resultCount = 1;
+    let page = 0;
+    const allJobs = [];
+
+    while (resultCount > 0) {
+      setIsLoading(true);
+      try {
+        const response = await axios.get(
+          `https://api.allorigins.win/get?url=${encodeURIComponent(
+            `https://jobs.github.com/positions.json?page=${page}&location=Germany`
+          )}`
+        );
+
+        const {
+          data: { contents },
+        } = response;
+
+        allJobs.push(...JSON.parse(contents));
+        resultCount = JSON.parse(contents).length;
+        page++;
+      } catch (error) {
+        setError(error);
+      }
+    }
+
+    setJobs(allJobs);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchGithub();
+  }, []);
+  console.log(jobs);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {isLoading && <p>IS LOADING...</p>}
+      {jobs?.length > 0 && map(jobs, (job, i) => <p key={i}>{job.title}</p>)}
     </div>
   );
-}
+};
 
 export default App;
