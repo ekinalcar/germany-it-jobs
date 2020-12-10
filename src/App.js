5bf66881d2b3a10e068e834b5418624492591aa1
Ekin Alçar <ekinalcar@gmail.com>
2020-12-10 20:42:14 +0100

    added styling to card and body

--- src/App.js
+++ src/App.js
@@ -41,17 +41,17 @@ const App = () => {
   useEffect(() => {
     fetchGithub();
   }, []);
-  console.log(jobs);
+
   return (
     <div className="sm:container sm:mx-auto p-5">
       {error && <p>ERROR ...</p>}
       {isLoading && <p>IS LOADING...</p>}
       {jobs?.length > 0 && (
-        <div class="grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-1 gap-5 ">
+        <div class="grid xl:grid-cols-3 lg:grid-cols-3 sm:grid-cols-1 gap-5">
           {map(jobs, (job, i) => (
             <div
               key={i}
-              className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl"
+              className="transition duration-500 ease-in-out transform hover:scale-105 cursor-pointer  max-w-md bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl bg-gray-50 p-2"
             >
               <div className="md:flex">
                 <div className="md:flex-shrink-0">
@@ -61,7 +61,7 @@ const App = () => {
                     alt="Man looking at item at a store"
                   />
                 </div>
-                <div className="px-2 py-4">
+                <div className="px-5">
                   <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
                     {job.title}
                   </div>
@@ -69,11 +69,11 @@ const App = () => {
                     href={job.url}
                     target="_blank"
                     rel="noreferrer"
-                    className="block mt-1 text-lg leading-tight font-medium text-black hover:underline"
+                    className="block mt-1 text-sm leading-tight font-medium text-black hover:underline"
                   >
                     {job.company}
                   </a>
-                  <p className="mt-2 text-gray-500"></p>
+                  <p className="mt-2 text-gray-500">{job.created_at}</p>
                 </div>
               </div>
             </div>
